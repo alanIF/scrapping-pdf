e08a443ef2e5a9eb8fe173a1c03988790e2a5a3d
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-23 12:21:43 -0300

    pdf open

--- main.py
+++ main.py
@@ -9,12 +9,28 @@ pdf_file = open('media/dados.pdf', 'rb')
 
 #Faz a leitura usando a biblioteca
 read_pdf = PyPDF2.PdfFileReader(pdf_file)
+
 # pega o numero de páginas
 number_of_pages = read_pdf.getNumPages()
-for i in range(0,number_of_pages):
-    #lê a primeira página completa
-    page =  read_pdf.getPage(i)  
-    text = page.extractText()
-    print(text)
-    
+
+#lê a primeira página completa
+page = read_pdf.getPage(0)
+
+#extrai apenas o texto
+page_content = page.extractText()
+
+# faz a junção das linhas 
+parsed = ''.join(page_content)
+
+print("Sem eliminar as quebras")
+print(parsed)
+
+# remove as quebras de linha
+parsed = re.sub('n', '', parsed)
+print("Após eliminar as quebras")
+print(parsed)
+
+print("nPegando apenas as 20 primeiras posições")
+novastring = parsed[0:20]
+print(novastring)
     
